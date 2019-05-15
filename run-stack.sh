f29cc16e88f9b933805516dcfa3ad9c323b71ac6
pravinsham <pravinsham@gmail.com>
2019-05-15 10:32:25 +0000

    increasing the cluster instance memory

--- run-stack.sh
+++ run-stack.sh
@@ -8,5 +8,5 @@ aws cloudformation $ACTION \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=272828950582.dkr.ecr.ap-south-1.amazonaws.com/example-web:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-30456358 \
-    ParameterKey=Cluster,ParameterValue=ecsintegrations \
+    ParameterKey=Cluster,ParameterValue=clusterwebapp \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:ap-south-1:272828950582:listener/app/productionintegration/1f82222f194c9522/dfc2213b0c3fc362
